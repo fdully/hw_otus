5abf7a80154cd4b972b477b5c3ca08648dcb2af9
fdully <fdully@gmail.com>
2020-03-22 08:54:32 +0300

    HW03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,54 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
+import (
+	"regexp"
+	"sort"
+	"strings"
+	"unicode/utf8"
+)
 
-func Top10(_ string) []string {
+func Top10(text string) []string {
 	// Place your code here
-	return nil
+	if text == "" {
+		return nil
+	}
+	if !utf8.ValidString(text) {
+		return nil
+	}
+
+	var (
+		wordCounter = map[string]int{}
+		uniq        []string
+		top         = 10
+		result      = make([]string, 0, top)
+	)
+
+	re := regexp.MustCompile(`\p{L}+-\p{L}+|\p{L}+| - |\t- |\n- `)
+	words := re.FindAllString(text, -1)
+
+	if len(words) == 0 {
+		return nil
+	}
+
+	if len(words) < top {
+		top = len(words)
+	}
+
+	for i, v := range words {
+		v = strings.TrimSpace(v)
+		words[i] = v
+		// selecting only uniq
+		if _, ok := wordCounter[v]; !ok {
+			uniq = append(uniq, v)
+		}
+		// counting words
+		wordCounter[v]++
+	}
+
+	sort.SliceStable(uniq, func(i, j int) bool {
+		return wordCounter[uniq[i]] > wordCounter[uniq[j]]
+	})
+
+	result = append(result, uniq[:top]...)
+
+	return result
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -43,9 +43,30 @@ var text = `Как видите, он  спускается  по  лестни�
 	посидеть у огня и послушать какую-нибудь интересную сказку.
 		В этот вечер...`
 
+const engText = `So this is bear and his name is Fuly. Behind him is nothing interesting and his name is Fuly.
+	So you'll see and hear. I don't now what else to say. But his name is Fuly and this is bear.'`
+
+const shortText = `Погода -8 да, сегодня.`
+
+var nonUtf8Text = string([]byte{0xff, 0xfe, 0xfd, 0x33, 0x3d})
+
 func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
 		assert.Len(t, Top10(""), 0)
+	})
+
+	t.Run("english language string", func(t *testing.T) {
+		expected := []string{"is", "and", "his", "name", "Fuly", "So", "this", "bear", "Behind", "him"}
+		assert.ElementsMatch(t, expected, Top10(engText))
+	})
+
+	t.Run("short string", func(t *testing.T) {
+		expected := []string{"Погода", "да", "сегодня"}
+		assert.ElementsMatch(t, expected, Top10(shortText))
+	})
+
+	t.Run("non utf8 string", func(t *testing.T) {
+		assert.Len(t, Top10(nonUtf8Text), 0)
 	})
 
 	t.Run("positive test", func(t *testing.T) {
@@ -53,7 +74,7 @@ func TestTop10(t *testing.T) {
 			expected := []string{"он", "а", "и", "что", "ты", "не", "если", "то", "его", "кристофер", "робин", "в"}
 			assert.Subset(t, expected, Top10(text))
 		} else {
-			expected := []string{"он", "и", "а", "что", "ты", "не", "если", "-", "то", "Кристофер"}
+			expected := []string{"он", "и", "а", "что", "ты", "не", "если", "-", "то", "его"}
 			assert.ElementsMatch(t, expected, Top10(text))
 		}
 	})
