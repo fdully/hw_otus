253271cb3699e44fd45a57b543899b9dd7aeefc1
fdully <fdully@gmail.com>
2020-05-16 11:27:13 +0300

    HW08_COMPLETED

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+const minimalArgs = 3
+
 func main() {
 	// Place your code here
+	if len(os.Args) < minimalArgs {
+		fmt.Println("Usage: go-envdir /path/to/env/dir shell-command arg1 arg2")
+	}
+
+	environmentFromFiles, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	commandEnvrionment := MakeCommandEnv(os.Environ(), environmentFromFiles)
+
+	os.Exit(RunCmd(os.Args[2:], commandEnvrionment))
 }
